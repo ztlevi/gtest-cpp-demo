f3191d2934e20b5adeb04dc332e07d79e2cb1c24
Ting Zhou <ztlevi.work@gmail.com>
2022-01-03 17:24:41 -0800

    add sample mock

---
+++ tests/gmock/mock_turtle.h
@@ -0,0 +1,13 @@
+#include "turtle.h"
+#include "gmock/gmock.h"  // Brings in gMock.
+
+class MockTurtle : public Turtle {
+ public:
+  MOCK_METHOD(void, PenUp, (), (override));
+  MOCK_METHOD(void, PenDown, (), (override));
+  MOCK_METHOD(void, Forward, (int distance), (override));
+  MOCK_METHOD(void, Turn, (int degrees), (override));
+  MOCK_METHOD(void, GoTo, (int x, int y), (override));
+  MOCK_METHOD(int, GetX, (), (const, override));
+  MOCK_METHOD(int, GetY, (), (const, override));
+};
